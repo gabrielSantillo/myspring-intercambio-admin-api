aa3970edd917830642c33a8a6ab64eb041205adf
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-04-12 11:30:07 -0400

    Adding the function to post a student

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from dbcreds import production_mode
 from flask_cors import CORS
-import endpoints.consultant, endpoints.consultant_login
+import endpoints.consultant, endpoints.consultant_login, endpoints.student
 
 # calling the Flask function which will return a value that will be used in my API
 app = Flask(__name__)
@@ -34,6 +34,12 @@ def login_consultant():
 @app.delete('/api/consultant_login')
 def delete_consultant_token():
     return endpoints.consultant_login.delete()
+
+
+########## STUDENT ##########
+@app.post('/api/student')
+def post_student():
+    return endpoints.student.post()
 
 
 # if statement to check if the production_mode variable is true, if yes, run in production mode, if not, run in testing mode
---
+++ endpoints/student.py
@@ -0,0 +1,25 @@
+from flask import request, make_response
+from apihelpers import check_endpoint_info, check_data_sent
+import json
+from dbhelpers import run_statement
+
+def post():
+    is_valid_header = check_endpoint_info(request.headers, ['token'])
+    if(is_valid_header != None):
+        return make_response(json.dumps(is_valid_header, default=str), 400)
+    
+    is_valid_data = check_endpoint_info(request.json,
+                                        ['first_name', 'last_name', 'email', 'phone_number', 'birth_date', 'marital_status'])
+    if(is_valid_data != None):
+        return make_response(json.dumps(is_valid_data, default=str), 400)
+    
+    results = run_statement('CALL add_student(?,?,?,?,?,?,?)', [
+        request.json.get('first_name'), request.json.get('last_name'), request.json.get('email'), request.json.get('phone_number'), request.json.get('birth_data'), request.json.get('marital_status'), request.headers.get('token')
+    ])
+
+     # if the response is a list and the length is different than zero send 200 as response
+    if (type(results) == list and len(results) != 0):
+        return make_response(json.dumps(results[0], default=str), 200)
+    # else a 500 as response
+    else:
+        return make_response(json.dumps(results[0], default=str), 500)
