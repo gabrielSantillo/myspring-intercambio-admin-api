fa05e629e80309d107fabc90707e5409e5e75495
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-04-12 15:45:21 -0400

    Adjusting the function to check if the request cames with a student id

--- endpoints/student.py
+++ endpoints/student.py
@@ -37,14 +37,14 @@ def get():
     if (is_valid_header != None):
         return make_response(json.dumps(is_valid_header, default=str), 400)
 
-    if (request.args.get != None):
+    if (request.args.get('id') != None):
         return
     else:
         results = run_statement('CALL get_all_students()')
 
         # if the response is a list and the length is different than zero send 200 as response
         if (type(results) == list and len(results) != 0):
-            return make_response(json.dumps(results[0], default=str), 200)
+            return make_response(json.dumps(results, default=str), 200)
         # else a 500 as response
         else:
             return make_response(json.dumps(results[0], default=str), 500)
