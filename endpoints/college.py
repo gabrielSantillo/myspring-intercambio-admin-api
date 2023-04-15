0f5c22f53a4e18986a34fb1515cd72219ddfde8a
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-04-15 11:40:36 -0400

    Adding a function that will delete a colelge

--- endpoints/college.py
+++ endpoints/college.py
@@ -36,3 +36,18 @@ def get():
                              400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+    
+
+def delete():
+    is_valid = check_endpoint_info(request.json, ['college_id'])
+    if(is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+    
+    results = run_statement('CALL delete_college(?)', [request.json.get('college_id')])
+
+    if(type(results) == list and results[0]['row_updated'] != 0):
+        return make_response(json.dumps(results[0], default=str), 200)
+    elif(type(results) == list and results[0]['row_updated'] == 0):
+        return make_response(json.dumps("Wrong college id", default=str), 400)
+    else:
+        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
