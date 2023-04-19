0f84a668e021a76914d2fddebc83fd1964546014
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-04-19 17:38:56 -0400

    Adding a function in the college file to update college info

--- endpoints/college.py
+++ endpoints/college.py
@@ -1,5 +1,5 @@
 from flask import request, make_response
-from apihelpers import check_endpoint_info, organize_college_response
+from apihelpers import check_endpoint_info, organize_college_response, check_data_sent
 import json
 from dbhelpers import run_statement
 
@@ -51,3 +51,33 @@ def delete():
         return make_response(json.dumps("Wrong college id", default=str), 400)
     else:
         return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+
+def patch():
+    is_valid_header = check_endpoint_info(request.headers, ['token'])
+    if (is_valid_header != None):
+        return make_response(json.dumps(is_valid_header, default=str), 400)
+    
+    is_valid_data = check_endpoint_info(request.json, ['id'])
+    if (is_valid_data != None):
+        return make_response(json.dumps(is_valid_data, default=str), 400)
+    
+    college_info = run_statement('CALL get_college_by_id(?,?)', [
+        request.json.get('id'), request.headers.get('token')])
+    
+    if (type(college_info) == list and len(college_info) != 0):
+        update_college_info = check_data_sent(request.json, college_info[0],
+                                              ['id', 'province_id', 'name'])
+
+        # calling the function that will edit a college
+        results = run_statement('CALL edit_college(?,?,?,?,?,?,?,?,?,?)',
+                                [update_college_info['id'], update_college_info['province_id'], update_college_info['name'], request.headers.get('token')])
+
+        # if the response is a list and the row_updated is equal than 1 send 200 as response
+        if (type(results) == list and results[0]['row_updated'] != 0):
+            return make_response(json.dumps(results[0], default=str), 200)
+        # if the response is a list and the row_updated is equal than 0 send 400 as response
+        elif (type(results) == list and results[0]['row_updated'] == 0):
+            return make_response(json.dumps(results[0], default=str), 400)
+        # else send 500 as an internal error
+        else:
+            return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
