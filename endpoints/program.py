dcf8e10880f2cebe85ceae9b12406bacb686d7d6
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-04-20 10:53:05 -0400

    Adding the function that will get all programs based on college id

--- endpoints/program.py
+++ endpoints/program.py
@@ -22,7 +22,23 @@ def get():
     if(request.args.get('student_id') != None):
         return
     elif(request.args.get('college_id') != None):
-        return
+        is_valid_data = check_endpoint_info(request.args, ['college_id'])
+        if(is_valid_data != None):
+            return make_response(json.dumps(is_valid_data, default=str), 400)
+
+        results = run_statement('CALL get_program_by_college_id(?)', [request.args.get('college_id')])
+
+        if(type(results) == list and len(results) != 0):
+            return make_response(json.dumps(results, default=str), 200)
+        elif(type(results) == list and len(results) == 0):
+            return make_response(json.dumps("Wrong college id", default=str), 400)
+        else:
+            return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
+
+        if(type(results) == list and len(results) != 0):
+            return make_response(json.dumps(results, default=str), 200)
+        else:
+            return make_response(json.dumps("Sorry, an error has occurred", default=str), 500)
     else:
         results = run_statement('CALL get_all_programs()')
 
